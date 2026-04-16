5c027b3c802a682b677686fb393c6b2dd02b160a
Ferch <chaconfernando3110@gmail.com>
2026-04-16 17:54:02 -0600

    comit

--- src/main/java/com/umg/simulador_mundial/repository/JugadorRepository.java
+++ src/main/java/com/umg/simulador_mundial/repository/JugadorRepository.java
@@ -7,13 +7,10 @@ import java.util.List;
 
 public interface JugadorRepository extends JpaRepository<Jugador, Long> {
     
-    // Algoritmo de Búsqueda: Busca jugadores que contengan el texto, ignorando mayúsculas/minúsculas
     List<Jugador> findByNombreContainingIgnoreCase(String nombre);
 
-    // Nos trae los goleadores ordenados de mayor a menor
     List<Jugador> findTop10ByOrderByGolesAnotadosDesc();
 
-    // Nos trae a los porteros ordenados por los que tienen MENOS goles recibidos
     List<Jugador> findTop10ByPosicionOrderByGolesRecibidosAsc(String posicion);
 
     List<Jugador> findByEquipo(Equipo equipo);
